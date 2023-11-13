3d50874c19ffd9226a7aeb68360b6f473a3d984c
Jubayer H Ahad <zubayerhossain111@gmail.com>
2023-11-13 21:22:36 +0100

    added init.py, setup.py and requirements.txt for pip installation

---
+++ math_quiz/__init__.py
@@ -0,0 +1,4 @@
+from .math_quiz import main
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_WS23_hw_01',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    author='Jubayer H Ahad',
+    author_email='jubayer.ahad@fau.de',
+    description='This is homework 2 for the DSSS course of winter 23 in FAU',
+    url='https://github.com/zubu007/dsss_WS23_hw_01',
+)
